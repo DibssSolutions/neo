25992f1da292507a18dfb76342550d6c0155b39d
Ihor Liubko <liub41k69@gmail.com>
2018-12-26 10:07:34 +0200

    repush

--- src/js/components/index.js
+++ src/js/components/index.js
@@ -1,10 +1,11 @@
 var acc = document.getElementsByClassName('accordion__question');
 var arrow = document.getElementsByClassName('accordion__arrow');
 var i;
+// querySelector 
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener('click', function() {
-    // arrow.classList.toggle('accordion__arrow_active');
+    arrow.classList.toggle('accordion__arrow_active');
     // this.classList.toggle('active');
     var panel = this.nextElementSibling;
     if (panel.style.display === 'block') {
@@ -19,4 +20,5 @@ var toggle = document.getElementsByClassName('header__toggle')[0];
 toggle.onclick = function() {
   var icon = document.getElementsByClassName('header__toggle-icon')[0];
   icon.classList.toggle('header__toggle-icon_active');
+
 };
